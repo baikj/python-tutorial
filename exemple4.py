a024c75b309fb7d7b76eb73b891f66de38640609
jay <ethiansjh@icloud.com>
2018-03-21 16:25:15 +0100

    exemple4

---
+++ exemple4.py
@@ -0,0 +1,16 @@
+# ouvrir le fichier data.txt
+# lire les valeurs
+# et mettre les valeurs dans un dictionnaire
+# trier le dictionnaire par valeur
+
+file = open('data.txt', 'r')
+text = file.read()
+dic = dict()
+list = text.split('\n')
+
+for i in list:
+    p = i.split(',')
+    dic[p[0]] = int(p[1])
+print(dic)
+
+file.close()
